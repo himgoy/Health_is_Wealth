8bba81a291999242e2c0ed6ab2b911399437344f
Himanshu Goyal <54285534+himgoy@users.noreply.github.com>
2021-05-06 19:15:22 +0530

    Add files via upload

---
+++ Healthy_Programmer.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from pygame import mixer
+from time import time
+
+
+def musicOnLoop(file, stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play(loops=1000)
+    while True:
+        a = input()
+        mixer.music.load(file)
+        mixer.music.play()
+        if a == stopper:
+            mixer.music.stop()
+            break
+
+
+def logNow(msg):
+    with open("log.txt", "rt") as f:
+        data = f.read()
+    with open("log.txt", "wt") as f:
+        f.write(f" {data}\n"
+                f"{msg} {datetime.now()}")
+
+
+if __name__ == '__main__':
+    # musicOnLoop("water.mp3", "stop")
+    init_water = time()
+    init_eyes = time()
+    init_exercise = time()
+    water_sec = 10
+    eyes_sec = 20
+    exercise_sec = 30
+    while True:
+        if time() - init_water > water_sec:
+            print("Kindly Drink some water in order to be Hydrated\n"
+                  "If drank, then write 'drank' on the console : ")
+            musicOnLoop("water.mp3", "drank")
+            init_water = time()
+            logNow("Drank water at ")
+
+        if time() - init_eyes > eyes_sec:
+            print("Kindly give some rest to you Eyes\n"
+                  "If rest is given, then write 'done' on the console : ")
+            musicOnLoop("eyes.mp3", "done")
+            init_eyes = time()
+            logNow("Eyes given rest at ")
+
+        if time() - init_exercise > exercise_sec:
+            print("Kindly do some exercise in order to be fit\n"
+                  "If you have completed the fitness, then write 'done' on the console : ")
+            musicOnLoop("exercise.mp3", "done")
+            init_exercise = time()
+            logNow("Exercised at ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
